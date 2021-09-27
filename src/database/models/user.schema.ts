92b931c06c327564528fe50efd40801bb97b0997
Стас <stasmq@gmail.com>
2021-09-27 16:35:48 +0300

    Created base structure2

---
+++ src/database/models/user.schema.ts
@@ -0,0 +1,58 @@
+import {Document, Model, model, Schema} from 'mongoose';
+import {IUser} from '../../models';
+
+export type UserType = IUser & Document
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const UserSchema: Schema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    default: 'user'
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: false
+  },
+  gender: {
+    type: String,
+    required: false
+  },
+  photo: {
+    type: String,
+    required: false
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'pending'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+export const User: Model<any> = model<UserType>('users', UserSchema);
